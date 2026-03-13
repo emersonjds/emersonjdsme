208005dc2eab42a2716ce149ad85ff83c96acfb7
Emerson Silva <emerson_jdss@hotmail.com>
2026-03-12 23:43:07 -0300

    change on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex items-center justify-center">
-      <p className="text-white text-2xl">Bem vindo</p>
+      <p className="text-white text-4xl font-bold">emersonjds</p>
     </div>
   )
 }
